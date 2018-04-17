9b8a10c341bb09cdf42725d8f6727121bbc49f2c
kanchanna <ramasamybalraj@gmail.com>
2018-04-17 19:05:08 -0400

    Update createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name "Ref": "KeyName" --security-group-ids   "Ref": "JenkinsSecurityGroup"
+aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name akey --region us-east-1
