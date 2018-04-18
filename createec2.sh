ab2b7a2d5f32b1c70d1e61cdc1dc8e124bc90e4b
kanchanna <ramasamybalraj@gmail.com>
2018-04-17 20:16:48 -0400

    Update createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name $1 --user-data file://my_script.txt ---region us-east-1
+aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name $1 --user-data file://myscript.txt ---region us-east-1
